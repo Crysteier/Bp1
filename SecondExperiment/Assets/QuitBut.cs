ac09a615c80daf1ed3787a540c2c8db92c909b91
Guntel <crysteier12345@gmail.com>
2021-02-18 20:07:44 +0100

    first commit

---
+++ SecondExperiment/Assets/QuitBut.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitBut : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("quitting");
+    }
+}
---
+++ SecondExperiment/Assets/Scripts/TriggerAnime.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TriggerAnime : MonoBehaviour
+{
+
+    Animation anime;
+    void Start()
+    {
+        anime = GetComponent<Animation>();
+        anime.Play("MoveForwardMeta");
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.tag == "wall")
+        {
+            anime.Stop("MoveForwardMeta");
+            anime.Play("Scene");
+        }
+
+        if (anime.isPlaying)
+        {
+          Debug.Log("help meeee");
+            StartCoroutine(ZaWarudo());
+           
+        }
+    }
+    IEnumerator ZaWarudo()
+    {
+        Debug.Log("time stopped");
+        yield return new WaitForSeconds(4);
+        Destroy(gameObject);
+
+    }
+
+}
